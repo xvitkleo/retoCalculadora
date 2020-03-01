6ff4356e29ab9aef5cdb3ef737f929d469004b85
Joaquin <xvitkleo@gmail.com>
2020-02-29 22:51:47 -0500

    mejoras en igual, operadores seguidos y cambiar signo

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let operando="";
 let historial="";
 let operar=false;
 let resultar=false;
+let operSeguido=false;
 let resultado;
 buttons.forEach(a => a.addEventListener('click', function() {    
     if(a.textContent=="AC") {
@@ -15,6 +16,11 @@ buttons.forEach(a => a.addEventListener('click', function() {
         resultar=false;
         resultado=0;
         display.textContent = 0;
+        historialDisplay.textContent=historial;
+    }
+    else if(a.textContent=="+/-") {
+        input = (parseInt(input)*-1).toString();
+        display.textContent = input;
         historialDisplay.textContent=historial;
     }
     else if("1234567890.".includes(a.textContent) && a.textContent!="") 
@@ -27,12 +33,9 @@ buttons.forEach(a => a.addEventListener('click', function() {
         input=input.concat(a.textContent);
         display.textContent = input;
         historialDisplay.textContent=historial;
+        operSeguido=false;
     }
-    else if(a.firstChild.className!="fas fa-equals"){ 
-        if(!("1234567890.".includes(historial.charAt(historial.length-1)))) historial=historial.replace(historial.charAt(historial.length-1), "");        
-        else if(input=="") input=input.concat("0");
-        historial=historial.concat(input);
-        input="";
+    else if(a.firstChild.className!="fas fa-equals"){                
         switch (a.firstChild.className) {
             case "fas fa-divide": 
                 operando="/";
@@ -49,20 +52,33 @@ buttons.forEach(a => a.addEventListener('click', function() {
             case "fas fa-percentage":
                 operando="%";
                 break;
-        }   
-        if(operar) 
-        {
-            resultado = eval(historial);
-            display.textContent = resultado;
+        }           
+        if(!operSeguido) {
+            operSeguido=true;                           
+            if(input=="") input=input.concat("0");
+            historial=historial.concat(input); 
+            input="";
+            if(operar) 
+            {
+                resultado = eval(historial);
+                display.textContent = resultado;
+            }        
+            operar=true;
+            historial=historial.concat(operando);
+            historialDisplay.textContent=historial; 
         }
-        operar=true;
-        historial=historial.concat(operando);
-        historialDisplay.textContent=historial;  
+        else {
+            historial=historial.substr(0, historial.length-1);
+            historial=historial.concat(operando);  
+            historialDisplay.textContent=historial; 
+        }
+         
         
     }
-    else {
+    else {        
         historial=historial.concat(input); 
         input="";
+        if(!"1234567890".includes(historial.charAt(historial.length-1))) historial=historial.concat(resultado.toString());
         resultado = eval(historial);
         display.textContent = resultado;
         historialDisplay.textContent=historial.concat("=");
